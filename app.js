5c6861b9abd2b6d21e22760a2c06ceb0db9b3435
pamanbobb <ngelurevolution5@gmail.com>
2022-11-04 01:21:13 +0700

    update

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require('express')
-const path = require('path')
 const ajax = require('./api/api')
 const app = express()
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
-})
-app.use('/log', ajax);
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.set('etag', false)
+app.use('/log', ajax)
+app.use(express.static('public'))
 
 var asu = process.env.PORT || 3000;
 app.listen(asu, ()=>{
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,22 @@
-$(()=>{
+$( () => {
+
+    let sukses = 0, gagal = 0;
+
+    $(document).on('input', 'textarea.user', function() {
+        let hitunghasil = $(this).val(), ete = $.trim(hitunghasil), lines = ete.split('\n');
+        hitung = lines.length;
+        $('.lab_user').text('Total Username : ' + hitung);
+    });
+
+    $(document).on('input', 'textarea.pass', function() {
+        let jumlahPassword = $(this).val(), words = jumlahPassword.split('\n');
+        getPass = words.length;
+        $('.lab_pass').text('Total Password : ' + getPass);
+    });
+
     $(document).on('click', 'button.elper', ()=> { 
-        var penguna = $('textarea.user').val().split('\n'), sandine = $('textarea.pass').val();
+        let penguna = $('textarea.user').val().split('\n'), sandine = $('textarea.pass').val().split('\n');
+
         if(!penguna || penguna < 1){
             $('.lab_user').text('Username Isi.');
             return false;
@@ -8,39 +24,95 @@ $(()=>{
             $('.lab_pass').text('Password isi.');
             return false;
         }else {
+            $('button.elper').text('Processing. Enteni Seg');
             $.each(penguna, (i, v) => {
-                setTimeout(() => {
-                    $.ajax({
-                        url: '/log',
-                        type: 'post',
-                        beforeSend: function(){
-                            $('button.elper').text('Enteni Seg');
-                            $('.inposition').text('Start Processing');
-                        },
-                        headers : {
-                            'content-type' : 'application/x-www-form-urlencoded'
-                        },
-                        data: {
-                            username : v,
-                            password : sandine
+                let io = true, pwe = i == penguna.length - 1;
+                $.each(sandine, (n, m) => {
+                    setTimeout(() => {
+                        if(pwe){
+                            //Last Execute
+                            $.ajax({
+                                url: '/log',
+                                type: 'post',
+                                cache: false,
+                                beforeSend: function(){
+                                    $('button.elper').text('Proccess Batch.');
+                                    $('.inposition').text('Login Terakhir Sebagai ' + v + ' ');
+                                },
+                                headers : {
+                                    'content-type' : 'application/x-www-form-urlencoded'
+                                },
+                                data: {
+                                    username : v,
+                                    password : m
+                                }
+                            }).done((data)=>{
+                                sukses++;
+                                $('textarea.cookies').sendkeys(data.cookie + '\n');
+                                $('.sukses').text(sukses).css('color', 'yellow');
+                                $('.inposition').text('Wes Entek !!');
+                                $('button.elper').text('Start Emulate');
+                            }).fail((xhr)=>{
+                                let logfail = JSON.parse(xhr.responseText);
+                                $('.gagal').text(gagal).css('color', 'red');
+                                $('.inposition').text('Wes Entek !!');
+                                $('button.elper').text('Start Emulate');
+                                if(logfail.status.indexOf('username') > - 1){
+                                    $('.inposition').text('Username Gak Ono.');
+                                }else if(logfail.status.indexOf('challenge_required') > - 1){
+                                    $('.inposition').text('Account Get code.');
+                                }else if(logfail.status.indexOf('password') > - 1){
+                                    $('.inposition').text('Sandi salah.');
+                                } else {
+                                    $('.inposition').text('Server Ig Error.');
+                                }
+                            })
+                        }else {
+                            //frist execute
+                            $.ajax({
+                                url: '/log',
+                                type: 'post',
+                                cache: false,
+                                beforeSend: function(){
+                                    $('button.elper').text('Proccess Batch.');
+                                    $('.inposition').text('Login Sebagai ' + v + '');
+                                },
+                                headers : {
+                                    'content-type' : 'application/x-www-form-urlencoded'
+                                },
+                                data: {
+                                    username : v,
+                                    password : m
+                                }
+                            }).done((data)=>{
+                                sukses++;
+                                $('textarea.cookies').sendkeys(data.cookie + '\n');
+                                $('.sukses').text(sukses).css('color', 'yellow');
+                                $('.inposition').text('Sukses Getting Cookie ' + v + '');
+                            }).fail((xhr)=>{
+                                let logfail = JSON.parse(xhr.responseText);
+                                gagal++;
+                                $('.gagal').text(gagal).css('color', 'red');
+                                if(logfail.status.indexOf('username') > - 1){
+                                    $('.inposition').text('Username Gak Ono.');
+                                }else if(logfail.status.indexOf('challenge_required') > - 1){
+                                    $('.inposition').text('Account Get code.');
+                                }else if(logfail.status.indexOf('password') > - 1){
+                                    $('.inposition').text('Sandi salah.');
+                                } else {
+                                    $('.inposition').text('Server Ig Error.');
+                                }
+                            })
                         }
-                    }).done((data)=>{
-                        $('textarea.cookies').val(data.cookie + '\n');
-                        $('.inposition').text('Sukses Getting Cookie!!');
-                        $('button.elper').text('Start Emulate');
-                    }).fail((xhr)=>{
-                        let logfail = JSON.parse(xhr.responseText);
-                        $('textarea.cookies').attr('placeholder', 'Failed !!');
-                        $('button.elper').text('Start Emulate');
-                        if(logfail.status.indexOf('username') > - 1){
-                            $('.inposition').text('Username Gak Ono.');
-                        }else if(logfail.status.indexOf('challenge_required') > - 1){
-                            $('.inposition').text('Account Get code.');
-                        } else {
-                            $('.inposition').text('Sandi salah.');
-                        }
-                    })
-                }, (i + 1) * 3000);
+                    }, (i + 1) * 20000);
+                });
+
+                if(pwe){
+                    io = false;
+                    return false;
+                }
+
+                return io;
             });
         }
     })
